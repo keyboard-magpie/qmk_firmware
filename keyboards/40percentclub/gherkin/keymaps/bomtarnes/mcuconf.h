8ef48975a7619d73f390eb73f3e6b5d3d17d0a47
daskygit <32983009+daskygit@users.noreply.github.com>
2022-03-24 20:26:53 +0000

    init

---
+++ keyboards/40percentclub/gherkin/keymaps/bomtarnes/mcuconf.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef STM32_PWM_USE_TIM2
+#define STM32_PWM_USE_TIM2 TRUE
+
+#undef STM32_PWM_USE_TIM3
+#define STM32_PWM_USE_TIM3 TRUE
+
+#undef STM32_PWM_USE_TIM4
+#define STM32_PWM_USE_TIM4 FALSE
+
+#undef STM32_ST_USE_TIMER
+#define STM32_ST_USE_TIMER 4
+
+#undef STM32_DAC_USE_DAC1_CH1
+#define STM32_DAC_USE_DAC1_CH1 TRUE
+
+#undef STM32_DAC_USE_DAC1_CH2
+#define STM32_DAC_USE_DAC1_CH2 TRUE
